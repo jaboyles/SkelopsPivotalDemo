3ec4afd05412c12a4e480004f00ed8e59dee7fd5
jaboyles <jacobboyles0115@gmail.com>
2018-03-15 11:08:00 -0400

    made comment worse

--- src/main/java/com/revature/controller/TRexController.java
+++ src/main/java/com/revature/controller/TRexController.java
@@ -24,7 +24,7 @@ public class TRexController {
 	@Autowired 
 	private TRexService trs;
 	
-	//This is a better comment
+	//This is a comment
 	@GetMapping("/all")
 	public ResponseEntity<List<TRex>> getAllTRexes() {
 		return new ResponseEntity<>(trs.getAllTRexes(), HttpStatus.OK);
