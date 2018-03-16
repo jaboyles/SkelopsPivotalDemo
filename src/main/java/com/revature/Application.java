83eb77fd27be2c133f482fe9eb4e7ea62b90e494
jaboyles <jacobboyles0115@gmail.com>
2018-03-16 09:13:57 -0400

    pls work

--- src/main/java/com/revature/Application.java
+++ src/main/java/com/revature/Application.java
@@ -1,27 +1,21 @@
 package com.revature;
 
-import java.io.IOException;
-
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.beans.RabbitReceiver;
-import com.revature.beans.TRex;
 
 @SpringBootApplication
 @RabbitListener(queues = "trex")
+@EnableEurekaClient
 @EnableScheduling
 public class Application extends SpringBootServletInitializer {
 	
