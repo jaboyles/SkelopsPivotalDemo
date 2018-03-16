27ea51cc51633f9888bf153af200d03e69548d93
jaboyles <jacobboyles0115@gmail.com>
2018-03-16 13:33:32 -0400

    pls work

--- src/main/java/com/revature/beans/RabbitReceiver.java
+++ src/main/java/com/revature/beans/RabbitReceiver.java
@@ -1,10 +1,15 @@
 package com.revature.beans;
 
+import java.io.IOException;
+
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.service.TRexService;
 
 @RabbitListener(queues = "trex")
@@ -14,7 +19,9 @@ public class RabbitReceiver {
 	TRexService rexService;
 	
 	@RabbitHandler
-	public void receive(@RequestBody TRex t) {
+	public void receive(String jsonTRex) throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		TRex t = mapper.readValue(jsonTRex, TRex.class);
 		rexService.addTRex(t);
 	}
 	
